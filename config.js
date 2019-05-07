34cca0abbc2519c0ab609f7032b9fe314e496a37
Daniel Devine <daniel@djdevine.co.uk>
2019-05-07 22:56:21 +0100

    Posts random weather images

---
+++ config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  consumer_key:         'WmmXZa71J8sD1ERch0uIWFa5K',
+  consumer_secret:      '5XTd2b1mLqoCxfCBVQPUk3yhaE8va5qNLCz72kMTsVNhXsH2FU',
+  access_token:         '1125141161218334720-g5S99ZYN6tRYNzcDChoI4mZVkOcvZv',
+  access_token_secret:  'C1w3PaRvVtOTE3xVkm3XxK7QnnAYrKCOeMBVAUtSn3Gtz',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
